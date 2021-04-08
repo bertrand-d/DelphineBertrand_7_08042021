77939212a4aefcb8feaca264562ac9267d5e077a
reuby-dev <delphineb57@gmail.com>
2021-04-08 17:24:53 +0200

    mise en place de l'environnement

---
+++ frontend/src/App.vue
@@ -0,0 +1,16 @@
+<script>
+import Connexion from './components/Connexion.vue'
+
+export default {
+	name: 'app',
+	components: {
+		Connexion
+	}
+}
+</script>
+
+<template>
+	<div id="app">
+		<Connexion></Connexion>
+	</div>
+</template>
---
+++ frontend/src/components/Connexion.vue
@@ -0,0 +1,21 @@
+<script>
+export default {
+	name: 'Connexion'
+}
+</script>
+
+<template>
+  <div class="connexion">
+    <form id="connexion__form">
+      <input id="email" type="email" name="email" />
+      <input id="password" type="password" name="password" />
+    </form>
+    <button class="connexion__button" type="button" form="connexion__form"> Connexion </button>
+  </div>
+</template>
+
+<style lang="scss">
+  .connexion__button {
+    color: $primary;
+  }
+</style>
