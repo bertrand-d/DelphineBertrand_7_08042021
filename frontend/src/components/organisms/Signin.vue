62d0d98481eb43ca1e280a5be2626d221f2b59ee
reuby-dev <delphineb57@gmail.com>
2021-04-13 17:06:27 +0200

    création du composant signin

---
+++ frontend/src/components/organisms/Signin.vue
@@ -0,0 +1,74 @@
+<script>
+import ButtonCard from '../atoms/ButtonCard.vue'
+import Input from '../atoms/Input.vue'
+import TextLink from '../atoms/TextLink.vue'
+
+export default {
+  name: 'Signin',
+  components: {
+    ButtonCard,
+    Input,
+    TextLink
+  }
+}
+</script>
+
+<template>
+    <div class= "signin">
+      <div class= "signin__body">
+            <div class ="signin__body-container">
+                <div class ="signin__body-input-container">
+                    <Input class="signin__input" type ="email" name="email" placeholder="E-mail"/>
+                    <Input class="signin__input" type ="password" name="password" placeholder="Mot de passe"/>
+                </div>
+                <div class="signin__body-input-container">
+                    <Input class="signin__input" type ="text" name="date-of-birth" placeholder="Date de naissance"/>
+                    <Input class="signin__input" type ="text" name="city" placeholder="Ville"/>
+                </div>
+            </div>
+            <div class= "signin__body-text-link-container">
+            <TextLink class="signin__text-link" url="/" text= "J'ai déjà un compte"/>
+            </div>
+      </div>
+        
+        <ButtonCard url ="/" text="inscription"/>
+    </div>
+ 
+</template>
+
+<style lang="scss">
+
+  .signin {
+    max-width: 650px;
+    border-radius: $border-radius-m;
+    box-shadow: $blur-shadow;
+    background-color: white;
+    text-align: center;
+
+    &__body {
+    padding: 40px 20px;
+    margin-right: -20px;
+    }
+  }
+
+  .signin__body-container {
+    display: flex;
+    flex-wrap: wrap;
+  }
+
+  .signin__body-input-container {
+      margin-right: 20px;
+      flex-grow: 1;
+      flex-basis: 0;
+      min-width: 150px;
+  }
+
+  .signin__input {
+    margin-bottom: 20px;
+  }
+
+  .signin__body-text-link-container {
+    margin-top :20px;
+  }
+
+</style>
--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,17 +1,35 @@
-<template>
-  <div class="home">
-    <Login></Login>
-  </div>
-</template>
-
 <script>
 
-import Login from '../components/organisms/Login'
+import Login from '../components/organisms/Login.vue'
+import Logo from '../components/atoms/Logo.vue'
 
 export default {
   name: 'Home',
   components: {
-    Login
+    Login,
+    Logo
   }
 }
 </script>
+
+<template>
+  <div class="home__container">
+    <div class="home__logo">
+      <Logo></Logo>
+    </div>
+      <Login></Login>
+  </div>
+</template>
+
+<style>
+.home__container {
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+}
+
+.logo {
+  max-width: 300px;
+}
+</style>
