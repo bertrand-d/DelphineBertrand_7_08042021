2bc52a775634359e1e2a4bc71c17834649bd024a
reuby-dev <delphineb57@gmail.com>
2021-04-18 19:53:13 +0200

    création de la view profile

---
+++ frontend/src/components/atoms/Button.vue
@@ -0,0 +1,34 @@
+<script>
+export default {
+  name: "ButtonCard",
+  props: ["text"],
+};
+</script>
+
+<template>
+    <div class="button">
+      <p class="button__text">{{ text }}</p>
+    </div>
+</template>
+
+<style lang="scss">
+.button {
+  background-color: $primary-color;
+  height: 80px;
+  border-radius: 20px;
+  line-height: 80px;
+  transition: background-color ease-in-out 0.25s;
+
+  &:hover {
+    background-color: darken($primary-color, 10%);
+  }
+
+  &__text {
+    text-align: center;
+    text-transform: uppercase;
+    line-height: 80px;
+    font-weight: $font-weight-bold;
+    color: white;
+  }
+}
+</style>
--- frontend/src/views/Profile.vue
+++ frontend/src/views/Profile.vue
@@ -1,12 +1,14 @@
 <script>
 import NavBar from "../components/organisms/NavBar.vue";
 import ProfileName from "../components/molecules/ProfileName.vue";
+import Button from "../components/atoms/Button.vue";
 
 export default {
   name: "Profile",
   components: {
     NavBar,
-    ProfileName
+    ProfileName,
+    Button
   },
 };
 </script>
@@ -14,9 +16,19 @@ export default {
 <template>
   <div id="profile">
     <NavBar></NavBar>
-    <ProfileName></ProfileName>
+    <div class="profile__container">
+      <ProfileName text="Walter"></ProfileName>
+      <Button text="Supprimer"></Button>
+    </div>
   </div>
 </template>
 
 <style lang='scss'>
+.profile__container {
+  @extend .max-container;
+  margin-top: 50px;
+  padding: 20px 60px;
+  border-radius: 20px;
+  background-color:white;
+}
 </style>
