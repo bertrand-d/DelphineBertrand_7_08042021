468824df69aaf776709c3b80ffeed93d81ec6114
reuby-dev <delphineb57@gmail.com>
2021-05-03 21:58:01 +0200

    rectification du signin

--- frontend/src/views/Signin.vue
+++ frontend/src/views/Signin.vue
@@ -2,12 +2,19 @@
 
 import SigninBox from '../components/organisms/SigninBox.vue'
 import Logo from '../components/atoms/Logo.vue'
+import router from '../router'
 
 export default {
   name: 'Signin',
   components: {
     SigninBox,
     Logo
+  },
+   beforeMount() {
+    if(sessionStorage.getItem("userId") && sessionStorage.getItem("token")) {
+      console.log("user is logged in");
+      router.push("NewsFeed");
+    }
   }
 }
 </script>
